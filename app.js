6b76fcb3f8262cdd3237b0a5328b09e16e38c22b
Dexter <dexter.t.jones@outlook.com>
2024-07-18 12:31:28 -0600

    Completed Triangle

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ var vertexShaderText =
 'precision mediump float;',
 '',
 'attribute vec2 vertPosition;',
+'attribute vec3 vertColor;',
+'varying vec3 fragColor;',
 '',
 'void main()',
 '{',
+'fragColor = vertColor;',
 ' gl_Position = vec4(vertPosition, 0.0, 1.0);',
 '}'
 ].join('\n');
@@ -14,9 +17,10 @@ var fragmentShaderText =
 [
 'precision mediump float;',
 '',
+'varying vec3 fragColor;',
 'void main()',
 '{',
-' gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);',
+' gl_FragColor = vec4(fragColor, 1.0);',
 '}'
 ].join('\n');
 var InitDemo = function() {
@@ -53,4 +57,51 @@ var InitDemo = function() {
         console.error('ERROR compiling fragment shader!', gl.getShaderInfoLog(fragmentShader));
         return;
     }
+
+    var program = gl.createProgram();
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+    gl.linkProgram(program);
+    if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+        console.error('Error linking program!', gl.getProgramInfoLog(program));
+        return;
+    }
+    // Create buffer
+    var triangleVertices = 
+    [ // x, y    R,G,B
+        0.0, 0.5,   1.0,1.0, 0.0,
+        -0.5, -0.5, 0.7, 0.0, 1.0,
+        0.5, -0.5,   0.1, 1.0, 0.6
+    ];
+
+    var triangleVertexBufferObject = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexBufferObject);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(triangleVertices), gl.STATIC_DRAW);
+
+    var positionAttribLocation = gl.getAttribLocation(program, 'vertPosition');
+    var colorAttribLocation = gl.getAttribLocation(program, 'vertColor');
+    gl.vertexAttribPointer(
+        positionAttribLocation, //Attribute Location
+        2, // Number of elements per attribute
+        gl.FLOAT, //Type of elements
+        gl.FALSE, //Not normalized
+        5 * Float32Array.BYTES_PER_ELEMENT, // Size of an individual vertex, float = 4 
+        0 // Offset from the beginning of a single vertex to this attribute
+    );
+    gl.vertexAttribPointer(
+        colorAttribLocation, //Attribute Location
+        3, // Number of elements per attribute
+        gl.FLOAT, //Type of elements
+        gl.FALSE, //Not normalized
+        5 * Float32Array.BYTES_PER_ELEMENT, // Size of an individual vertex, float = 4 
+        2 * Float32Array.BYTES_PER_ELEMENT// Offset from the beginning of a single vertex to this attribute
+    );
+
+    gl.enableVertexAttribArray(positionAttribLocation);
+    gl.enableVertexAttribArray(colorAttribLocation);
+
+    //Main render loop
+
+    gl.useProgram(program);
+    gl.drawArrays(gl.TRIANGLES, 0, 3);
 };
